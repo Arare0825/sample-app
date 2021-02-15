b6218d50cc31cdca97f7cb94b0b929f2fadff8ea
本郷哲太 <zhetaibenxiang@gmail.com>
2021-02-16 00:33:37 +0900

    no message

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -83,29 +83,29 @@ public function redirectToGithub()
     return Socialite::driver('github')->redirect();
 }
 
-public function handleGithubCallback()
-{
-    $fUser = Socialite::driver('github')->stateless()->user();
-    // email が合致するユーザーを取得
-    $user = User::where('email', $fUser->email)->first();
-    // 見つからなければ新しくユーザーを作成
-    if ($user == null) {
-        $user = $this->createUserByGithub($fUser);
-    }
-    // ログイン処理
-    \Auth::login($user, true);
-    return redirect('/');
-}
+// public function handleGithubCallback()
+// {
+//     $fUser = Socialite::driver('github')->stateless()->user();
+//     // email が合致するユーザーを取得
+//     $user = User::where('email', $fUser->email)->first();
+//     // 見つからなければ新しくユーザーを作成
+//     if ($user == null) {
+//         $user = $this->createUserByGithub($fUser);
+//     }
+//     // ログイン処理
+//     \Auth::login($user, true);
+//     return redirect('/');
+// }
 
-public function createUserByGithub($fUser)
-{
-    $user = User::create([
-        'name'     => $fUser->name,
-        'email'    => $fUser->email,
-        'password' => \Hash::make(uniqid()),
-    ]);
-    return $user;
-}
+// public function createUserByGithub($fUser)
+// {
+//     $user = User::create([
+//         'name'     => $fUser->name,
+//         'email'    => $fUser->email,
+//         'password' => \Hash::make(uniqid()),
+//     ]);
+//     return $user;
+// }
 
 // public function signin()
 //     {
