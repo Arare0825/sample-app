e39a9c7662afb281af3386db03e3edb02fb05929
本郷哲太 <zhetaibenxiang@gmail.com>
2021-02-17 10:38:42 +0900

    no message

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -79,34 +79,6 @@ class LoginController extends Controller
 
 
 
-//github認証
-// public function redirectToGithub()
-// {
-//     return Socialite::driver('github')->redirect();
-// }
-
-// public function handleGithubCallback()
-// {
-//     $fUser = Socialite::driver('github')->stateless()->user();
-//     // email が合致するユーザーを取得
-//     $user = User::where('email', $fUser->email)->first();
-//     // 見つからなければ新しくユーザーを作成
-//     if ($user == null) {
-//         $user = $this->createUserByGithub($fUser);
-//     }
-//     // ログイン処理
-//     \Auth::login($user, true);
-//     return redirect('/');
-// }
-
-// public function createUserByGithub($fUser)
-// {
-//     $user = User::create([
-//         'name'     => $fUser->name->nullable(),
-//         'email'    => $fUser->email->nullable(),
-//         'password' => \Hash::make(uniqid()),
-//     ]);
-//     return $user;
 
 public function redirectToGithub()
   {
